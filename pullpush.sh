5b0322a8f37855b59c9735da3185c049a620efd3
martin <martin@sirenia.eu>
2018-06-01 14:55:35 +0200

    remove pp

--- pullpush.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-git remote add vs https://robot:7ij4pd6bqmikip4ixn7hgw4mbkqgrcgrfgvilqulzy6rwhvsjn4a@automation-demo.visualstudio.com/hc21/_git/hc21
-git remote add github https://sireniabot:MQJHtOk57Jydn8qqt9De@github.com/sireniaeu/hc21.git
-while true
-  do
-    git pull vs master && git push github master
-    git pull github master && git push vs master
-done
