f7ac1470c66b27d370804885ac692b10c33297f7
kwanza <kwanza@sirenia.eu>
2018-06-04 09:37:25 +0000

    Committed via WithdrawByID invocation

--- flow/bbt9oq6flrd0009cjif0/descriptive/code.js
+++
@@ -1,9 +0,0 @@
-var reply = Http.post(
-  "http://localhost:9009/v1/ContextAction/Perform/hello.hello.hello",
-  "{'[eu.sirenia]Action.In.PsykAR.Cpr': '012345-176x'}", {
-    headers: {
-      'Authorization': "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJwc3lrYXIiLCJleHAiOjEwMDAxNTI1NDI3Nzc2LCJpYXQiOjE1MjU0Mjc3NzcsImlzcyI6IkpvbmF0aGFuIC8gU2lyZW5pYSIsIm5hbWUiOiJGcmVkZHlzIHRva2VuIiwiZXB0cyI6WyJDb250ZXh0QWN0aW9uL1BlcmZvcm0vKiJdfQ.K49J5NM7d7hplizAyp60BkOdBBdqNoNeztQSPQSbA8hOJ8Cmr_na_blvG5iTB1QV8B6I0_gmSE1jhKLt6Edmlg"
-    },
-    contenttype: "application/json",
-    timeout: 2000
-  });
