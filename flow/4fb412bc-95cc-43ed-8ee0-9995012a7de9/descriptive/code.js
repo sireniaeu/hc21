b9f7a91a3fc5cd2f50cb7da6283dddc0a0b9e189
kwanza <kwanza@sirenia.eu>
2018-06-04 09:27:40 +0000

    Committed via WithdrawByID invocation

--- flow/4fb412bc-95cc-43ed-8ee0-9995012a7de9/descriptive/code.js
+++
@@ -1,7 +0,0 @@
-var task = Http.getAsync("http://sirenia.eu", {});
-Debug.showDialog("Task done? "+task.done);
-if(task.wait(10000)) {
-  Debug.showDialog("Task done? "+task.done + " - "+task.result.status);
-} else {
-  Debug.showDialog("Task !done");
-}
