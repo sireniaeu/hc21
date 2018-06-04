202f61e8cde1a2fa0cda5eb4fce3829f0821e555
kwanza <kwanza@sirenia.eu>
2018-06-04 09:34:46 +0000

    Committed via WithdrawByID invocation

--- flow/c7f119a8-8856-4961-8b96-c957165bc635/descriptive/code.js
+++
@@ -1,9 +0,0 @@
-Notification.show("hello", "Hello", "I should be around for ~5 seconds", { timeout: 5 });
-Wait.forSeconds(6);
-Notification.show("hello", "Hello again", "This is my initial text", {});
-Wait.forSeconds(3);
-Notification.update("hello-embed", "Hello again", "This is some other text", { severity: "ERROR", embed: true });
-Wait.forSeconds(2);
-Notification.update("hello-embed-another", "Hello again", "This is some other text", { severity: "WARN", embed: true });
-Notification.show("hello-another", "Hello again", "This is my initial text", {});
-//Notification.close("hello");
