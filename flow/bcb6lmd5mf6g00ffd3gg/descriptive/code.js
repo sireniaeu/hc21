6cb5c58702f872ad55ab33e36e310268fc6cd04d
martin <martin@sirenia.eu>
2018-06-05 13:32:39 +0200

    ds

--- flow/bcb6lmd5mf6g00ffd3gg/descriptive/code.js
+++ flow/bcb6lmd5mf6g00ffd3gg/descriptive/code.js
@@ -2,5 +2,14 @@
 Fields["tree_sln_explorer"].highlight();
 Fields["tree_sln_explorer"].click();
 Window.sendKeys("{LEFT 10}");
+Window.sendKeys("flow");
+Window.sendKeys("{ENTER}");
+Window.sendKeys("bcb54");
+Window.sendKeys("{ENTER}");
+Window.sendKeys("descript");
+Window.sendKeys("{ENTER}");
+Window.sendKeys("type");
+Window.sendKeys("{ENTER}");
+
 //Wait.forMilliseconds(2000);
 Debug.ger(Fields["tree_sln_explorer"].inspect({ reflectionDepth: 2 }));
