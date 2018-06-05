6d7d22e30a9839366ea9f00908d8a8ef06ea9814
kwanza <kwanza@sirenia.eu>
2018-06-05 14:38:12 +0000

    Committed via Provide invocation

--- flow/bcb9u66p6eb000euu1bg/descriptive/code.js
+++ flow/bcb9u66p6eb000euu1bg/descriptive/code.js
@@ -1,9 +1,19 @@
 var t;
 
+function listsRepeat(value, n) {
+  var array = [];
+  for (var i = 0; i < n; i++) {
+    array[i] = value;
+  }
+  return array;
+}
+
 
 for (var count = 0; count < 10; count++) {
   t = null.read().inspect({});
 }
+
+listsRepeat(null, 5);
 
 null.editCell('', '', '', {});
 null.editCell('', '', '', {});
