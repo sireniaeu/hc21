2262b9f85d5732043f1900d5e9ebed51193b0922
martin <martin@sirenia.eu>
2018-06-05 13:24:37 +0200

    s

--- flow/bcb6lmd5mf6g00ffd3gg/descriptive/code.js
+++ flow/bcb6lmd5mf6g00ffd3gg/descriptive/code.js
@@ -1,6 +1,6 @@
 //My test flow
 Fields["tree_sln_explorer"].highlight();
 Fields["tree_sln_explorer"].click();
-Window.sendKeys("{LEFT}10");
+Window.sendKeys("{LEFT 10}");
 //Wait.forMilliseconds(2000);
 Debug.ger(Fields["tree_sln_explorer"].inspect({ reflectionDepth: 2 }));
