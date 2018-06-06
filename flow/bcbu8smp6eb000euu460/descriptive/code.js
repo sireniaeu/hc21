cd92025ba8f05bdf3195d868b2185fbb1481e7ae
kwanza <kwanza@sirenia.eu>
2018-06-06 13:45:47 +0000

    Committed via Provide invocation

--- flow/bcbu8smp6eb000euu460/descriptive/code.js
+++ flow/bcbu8smp6eb000euu460/descriptive/code.js
@@ -8,4 +8,4 @@ Window.sendKeys("%t");
 //select options
 Window.sendKeys("o");
 //Select Env/Keyboard
-Fields["tree_options_tree"].select("Env*/Keyboard");
+Fields["tree_options_tree"].select("Env*/Quick");
