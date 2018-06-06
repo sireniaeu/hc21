2f4058e0c9719490901fd59e26bb72d6bc98805a
martin <martin@sirenia.eu>
2018-06-06 10:40:54 +0200

    s

--- flow/bcb6lmd5mf6g00ffd3gg/descriptive/code.js
+++ flow/bcb6lmd5mf6g00ffd3gg/descriptive/code.js
@@ -1,39 +1,7 @@
 //My test flow
 Fields["tree_sln_explorer"].highlight();
-Fields["tree_sln_explorer"].click();
-Window.sendKeys("{LEFT}");
-Wait.forMilliseconds(100);
-Window.sendKeys("{LEFT}");
-Wait.forMilliseconds(100);
-Window.sendKeys("{LEFT}");
-Wait.forMilliseconds(100);
-Window.sendKeys("{LEFT}");
-Wait.forMilliseconds(100);
-Window.sendKeys("{LEFT}");
-Wait.forMilliseconds(100);
-Window.sendKeys("{LEFT}");
-Wait.forMilliseconds(100);
-Window.sendKeys("{LEFT}");
-Wait.forMilliseconds(100);
-Window.sendKeys("{LEFT}");
-Wait.forMilliseconds(100);
-Window.sendKeys("{LEFT}");
-Wait.forMilliseconds(100);
-Window.sendKeys("{LEFT}");
-Window.sendKeys("{ENTER}");
-Wait.forMilliseconds(1000);
-Window.sendKeys("flow");
-Window.sendKeys("{ENTER}");
-Wait.forMilliseconds(100);
-Window.sendKeys("bcb54");
-Window.sendKeys("{ENTER}");
-Wait.forMilliseconds(1000);
-Window.sendKeys("descript");
-Window.sendKeys("{ENTER}");
-Wait.forMilliseconds(1000);
-Window.sendKeys("app");
-Wait.forMilliseconds(500);
-Window.sendKeys("{ENTER}");
+Fields["tree_sln_explorer"].select("**/application");
+
 
 //Wait.forMilliseconds(2000);
 Debug.ger(Fields["tree_sln_explorer"].inspect({ reflectionDepth: 2 }));
