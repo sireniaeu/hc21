fc5374670188b21ea25ab72201b9dc45a2a2645d
kwanza <kwanza@sirenia.eu>
2018-06-06 11:07:16 +0000

    Committed via Provide invocation

--- flow/bcbrp4ep6eb000euu270/descriptive/code.js
+++ flow/bcbrp4ep6eb000euu270/descriptive/code.js
@@ -1,3 +1,5 @@
 Fields["list_list"].highlightWithColor("blue");
 //Debug.ger(Fields["list_list"].inspect());
 Fields["list_list"].select("Options");
+Fields["list_list"].select("Options");
+Fields["list_list"].select("Options");
