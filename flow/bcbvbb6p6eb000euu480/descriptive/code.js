8c823b420f3d1d9f3c9bccac5655999a2a01b7a3
kwanza <kwanza@sirenia.eu>
2018-06-06 15:07:46 +0000

    Committed via Provide invocation

--- flow/bcbvbb6p6eb000euu480/descriptive/code.js
+++ flow/bcbvbb6p6eb000euu480/descriptive/code.js
@@ -1,4 +1,4 @@
 //Ensure that the user is logged in
 Wait.forField(Fields["btn_pt_search"], 20);
 Fields["btn_pt_search"].click();
-Wait.forField(Fields["btn_pt_search"], 20);
+Wait.forField(Fields["input_ssn_search"], 20);
