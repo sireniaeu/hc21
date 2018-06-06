ddc9e9b9320601c2e23df2a41982c60b4f07d1dc
martin <martin@sirenia.eu>
2018-06-06 10:54:54 +0200

    d

--- flow/bcb6lmd5mf6g00ffd3gg/descriptive/code.js
+++ flow/bcb6lmd5mf6g00ffd3gg/descriptive/code.js
@@ -1,7 +1,7 @@
 //My test flow
 Fields["tree_sln_explorer"].highlight();
-Fields["tree_sln_explorer"].select("**/application*");
+//Fields["tree_sln_explorer"].select("**/application*");
 
 
 //Wait.forMilliseconds(2000);
-Debug.ger(Fields["tree_sln_explorer"].inspect({ reflectionDepth: 2 }));
+Debug.ger(Fields["tree_sln_explorer"].inspect());
