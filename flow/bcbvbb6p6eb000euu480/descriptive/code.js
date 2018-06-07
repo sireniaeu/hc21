3d8a2718e7743db0cb07d9c5b680f3f27674eed0
kwanza <kwanza@sirenia.eu>
2018-06-07 07:58:20 +0000

    Committed via Provide invocation

--- flow/bcbvbb6p6eb000euu480/descriptive/code.js
+++ flow/bcbvbb6p6eb000euu480/descriptive/code.js
@@ -10,7 +10,7 @@ lookupPatient();
 //Function
 function ensureLoggedIn() {
     try {
-    //Test if login
+    //Test if loginprompt is open
     Wait.forField(Fields["input_ssn_search"], 1);
   } catch (e) {
     Wait.forField(Fields["btn_pt_search"], 20);
