537a8fd232a21b4d62dffae7c01128063af91477
kwanza <kwanza@sirenia.eu>
2018-06-07 11:46:41 +0000

    Committed via Provide invocation

--- flow/bcbvbb6p6eb000euu480/descriptive/code.js
+++ flow/bcbvbb6p6eb000euu480/descriptive/code.js
@@ -22,8 +22,6 @@ if (isLoggedIn()) {
 //Function
 function isLoggedIn() {
   try {
-    //Test if loginprompt is open
-    Fields["input_username"].exists();
     //We are at login prompt
     Dialog.info('Login', 'You need to be logged in. Log in and run again.', {
       'timeout': 10
