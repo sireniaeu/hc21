86647d2f9b0fc274f25b2cfa4e15e5cef2f3c5af
kwanza <kwanza@sirenia.eu>
2018-06-07 08:00:59 +0000

    Committed via Provide invocation

--- flow/bcbvbb6p6eb000euu480/descriptive/code.js
+++ flow/bcbvbb6p6eb000euu480/descriptive/code.js
@@ -38,5 +38,6 @@ function ensurePatientFinderOpen() {
 //Function
 function lookupPatient() {
   //Fields["input_ssn_search"].input("121212-1212");
+  
   Fields["input_ssn_search"].inputNative(" ");
 }
