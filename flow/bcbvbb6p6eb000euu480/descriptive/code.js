ff1c4ea79e615934b1f4994adc5f209019b26cfd
kwanza <kwanza@sirenia.eu>
2018-06-07 07:41:06 +0000

    Committed via Provide invocation

--- flow/bcbvbb6p6eb000euu480/descriptive/code.js
+++ flow/bcbvbb6p6eb000euu480/descriptive/code.js
@@ -3,3 +3,4 @@ Wait.forField(Fields["btn_pt_search"], 20);
 Fields["btn_pt_search"].click();
 //Wait.forField(Fields["input_ssn_search"], 3);
 Fields["input_ssn_search"].inputNative("121212-1212");
+Fields["input_ssn_search"].inputNative("121212-1212");
