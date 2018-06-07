753655cbddf38ee8d52e9604ed28999487fe333e
kwanza <kwanza@sirenia.eu>
2018-06-07 07:50:20 +0000

    Committed via Provide invocation

--- flow/bcbvbb6p6eb000euu480/descriptive/code.js
+++ flow/bcbvbb6p6eb000euu480/descriptive/code.js
@@ -2,7 +2,7 @@
 //Wait.forField(Fields["btn_pt_search"], 20);
 //Fields["btn_pt_search"].click();
 
-//Ensure 
+//Ensure Patient Finder is open
 
 Wait.forField(Fields["input_ssn_search"], 3);
 //Wait.forMilliseconds(3000);
