0e201ac5f9fb08cc8802938006fb6b18ede9e3be
kwanza <kwanza@sirenia.eu>
2018-06-11 13:10:09 +0000

    Committed via Provide invocation

--- flow/bcbu8smp6eb000euu460/descriptive/code.js
+++ flow/bcbu8smp6eb000euu460/descriptive/code.js
@@ -8,8 +8,8 @@ Window.sendKeys("%t");
 //select options
 Window.sendKeys("o");
 //Select Env/Keyboard
-Fields["tree_options_tree"].select("Env*/Quick*");
+Fields["tree_options_tree"].select("Environment/Quick*");
 //List select Options
 Fields["list_list"].select("Options");
 //Cleanup
-Fields["tree_options_tree"].select("Env*");
+//Fields["tree_options_tree"].select("Env*");
