7159a1470c6e794bc80ae5876fcdc6986f30711f
kwanza <kwanza@sirenia.eu>
2018-06-11 13:14:15 +0000

    Committed via Provide invocation

--- flow/bcbu8smp6eb000euu460/descriptive/code.js
+++ flow/bcbu8smp6eb000euu460/descriptive/code.js
@@ -7,6 +7,7 @@ Wait.forMilliseconds(1000);
 Fields["tree_options_tree"].select("Environment/Quick*", {expand:true});
 //List select Options
 Fields["list_list"].select("Options");
+Wait.forMilliseconds(1000);
 // Deselect option
 Window.sendKeys("-", {focus:false});
 //Cleanup
