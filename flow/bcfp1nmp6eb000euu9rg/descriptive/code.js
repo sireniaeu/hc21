652a6ea08c5ffe9d276a306dfe5a61fe7d1f670e
kwanza <kwanza@sirenia.eu>
2018-06-12 09:56:25 +0000

    Committed via Provide invocation

--- flow/bcfp1nmp6eb000euu9rg/descriptive/code.js
+++ flow/bcfp1nmp6eb000euu9rg/descriptive/code.js
@@ -1,4 +1,5 @@
 Fields["bnt_purchase"].click({});
 Window.sendKeys("+^o");
 Fields["table_datagrid"].highlight();
-Debug.ger(Fields["table_datagrid"].inspect());
+Wait.forMilliseconds();
+Debug.ger(Fields["table_datagrid"]);
