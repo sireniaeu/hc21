7aae027abb0d8a2e9cfbf2f9301e1705e3f411fa
kwanza <kwanza@sirenia.eu>
2018-06-12 13:54:45 +0000

    Committed via Provide invocation

--- flow/bcfscq6p6eb000euuc8g/descriptive/code.js
+++ flow/bcfscq6p6eb000euuc8g/descriptive/code.js
@@ -4,7 +4,7 @@ Fields["btn_displayRecord"].click();
 Wait.forMilliseconds(500);
 Fields["btn_cancelRecord"].click();
 Wait.forMilliseconds(1500);
-Window.sendKeys("{ENTER}");
+Window.sendKeys("{ENTER}", {focus:false});
 Wait.forMilliseconds(500);
 Window.sendKeys("{ENTER}", {focus:false});
 Fields["btn_displayReport"].click();
