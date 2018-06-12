35967092d1cffd810d2b2b4675ac9f9cc2d16879
kwanza <kwanza@sirenia.eu>
2018-06-12 12:33:56 +0000

    Committed via Provide invocation

--- flow/bcfp1nmp6eb000euu9rg/descriptive/code.js
+++ flow/bcfp1nmp6eb000euu9rg/descriptive/code.js
@@ -34,7 +34,7 @@ Window.sendKeys("{ENTER}");
 
 Window.sendKeys("Two of these");
 Window.sendKeys("{TAB}");
-Window.sendKeys("1");
+Window.sendKeys("2");
 Window.sendKeys("{TAB}");
 Window.sendKeys("100");
 Window.sendKeys("{ENTER}");
