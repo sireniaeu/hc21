1c760b479d64ddb203f3b2dfbcabbad13349a4b9
kwanza <kwanza@sirenia.eu>
2018-06-12 12:58:08 +0000

    Committed via Provide invocation

--- flow/bcfp1nmp6eb000euu9rg/descriptive/code.js
+++ flow/bcfp1nmp6eb000euu9rg/descriptive/code.js
@@ -3,7 +3,7 @@ args = {};
 args.ref_no = Date.now();
 args.add_to_address = true;
 args.remarks = "Something here. " + Date.now();
-args.items = [{name:"One of these", amount:1, price:}, "Two of these", "Some of those"];
+args.items = [{name:"One of these", amount:1, price:100}, "Two of these", "Some of those"];
 
 
 //Click po menu
