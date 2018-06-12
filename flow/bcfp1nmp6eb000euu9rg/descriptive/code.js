dcb85aaaa5813ae7f88a756bf11e29de6c4944bb
kwanza <kwanza@sirenia.eu>
2018-06-12 11:57:47 +0000

    Committed via Provide invocation

--- flow/bcfp1nmp6eb000euu9rg/descriptive/code.js
+++ flow/bcfp1nmp6eb000euu9rg/descriptive/code.js
@@ -33,7 +33,7 @@ Fields["input_RefNotextBox"].input(args.ref_no);
 
 //Add Add to another add
 Fields["input_checkBox_Add_Address"].highlight();
-Fields["input_checkBox_Add_Address"].input(args.);
+Fields["input_checkBox_Add_Address"].input(add_to_address);
 
 
 //Execute po
