06f9bf949e675224535278df8452463b21b6af2e
kwanza <kwanza@sirenia.eu>
2018-06-12 12:30:47 +0000

    Committed via Provide invocation

--- flow/bcfp1nmp6eb000euu9rg/descriptive/code.js
+++ flow/bcfp1nmp6eb000euu9rg/descriptive/code.js
@@ -23,6 +23,9 @@ Fields.btn_create_po.click();
 //Add item lines. TODO:
 Fields["table_po_datagrid"].highlight();
 //Debug.ger(Fields["table_po_datagrid"]);
+Fields["table_po_datagrid"].click();
+Fields["table_po_datagrid"].highlight();
+
 
 //Add ref
 //Fields["input_RefNotextBox"].highlight();
