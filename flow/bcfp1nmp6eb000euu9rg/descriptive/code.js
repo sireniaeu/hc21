a959aeea1a975c870ecde6c55eb7ac00241dfa4a
kwanza <kwanza@sirenia.eu>
2018-06-12 11:21:20 +0000

    Committed via Provide invocation

--- flow/bcfp1nmp6eb000euu9rg/descriptive/code.js
+++ flow/bcfp1nmp6eb000euu9rg/descriptive/code.js
@@ -2,5 +2,5 @@ Fields["bnt_purchase"].click({});
 Window.sendKeys("+^o");
 Fields["table_datagrid"].highlight();
 Wait.forMilliseconds(1000);
-Debug.ger(Fields["table_datagrid"]);
+//Debug.ger(Fields["table_datagrid"]);
 Fields.btn_create_po.click();
