c64c3bf39a8904a578a43193af8687a3b54eaedc
kwanza <kwanza@sirenia.eu>
2018-06-13 09:32:27 +0000

    Committed via Provide invocation

--- flow/bcftjp6p6eb000euucig/descriptive/code.js
+++ flow/bcftjp6p6eb000euucig/descriptive/code.js
@@ -1,6 +1,7 @@
 for (var count = 0; count < 10; count++) {
   Notification.show('info', 'Its complicated', 'Round '+count, { severity: 'INFO', timeout: 5 });
   if(Fields["btn_Message_ok"].exists()){
+     Notification.show('info', 'Its complicated', 'Round '+count, { severity: 'INFO', timeout: 5 });
     Fields["btn_Message_ok"].highlight();
     break;
   }
