77170eec5f5aeffea964d6f863890c36c4243165
kwanza <kwanza@sirenia.eu>
2018-06-13 11:35:12 +0000

    Committed via Provide invocation

--- flow/bcftjp6p6eb000euucig/descriptive/code.js
+++ flow/bcftjp6p6eb000euucig/descriptive/code.js
@@ -5,7 +5,7 @@ for (var count = 0; count < 2; count++) {
   Notification.show('info', 'Its complicated', 'Round '+count, { severity: 'INFO', timeout: 5 });
   if(Fields["btn_Message_ok"].exists()){
     Notification.show('info', 'All POs have been cancelled', '', { severity: 'INFO', timeout: 5 });
-    Fields["btn_Message_ok"].highlight();
+    //Fields["btn_Message_ok"].highlight();
     break;
   }
   Flow.run("Cancel Record", {});
