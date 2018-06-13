80c1a8d84e2b2cc1c41a534ba19f6828f51cc4be
kwanza <kwanza@sirenia.eu>
2018-06-13 09:26:41 +0000

    Committed via Provide invocation

--- flow/bcftjp6p6eb000euucig/descriptive/code.js
+++ flow/bcftjp6p6eb000euucig/descriptive/code.js
@@ -1,7 +1,7 @@
 for (var count = 0; count < 10; count++) {
   if(Fields["btn_Message_ok"].exists()){
-    break;
     Fields["btn_Message_ok"].highlight();
+    break;
   }
   //Flow.run("Cancel Record", {});
 }
