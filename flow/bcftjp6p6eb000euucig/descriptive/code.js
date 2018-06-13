54ff43d67ccfbe3e29a846d9d44e7da3b7430201
kwanza <kwanza@sirenia.eu>
2018-06-13 11:35:42 +0000

    Committed via Provide invocation

--- flow/bcftjp6p6eb000euucig/descriptive/code.js
+++ flow/bcftjp6p6eb000euucig/descriptive/code.js
@@ -1,5 +1,9 @@
 //Debug.ger(Fields["btn_Message_ok"].inspect());
 //Fields["btn_Message_ok"].highlight();
+Fields["list_select_status"].select("Canceled");
+Wait.forMilliseconds(1000);
+Fields["btn_displayReport"].click();
+Wait.forMilliseconds(500);
 
 for (var count = 0; count < 2; count++) {
   Notification.show('info', 'Its complicated', 'Round '+count, { severity: 'INFO', timeout: 5 });
