3150ae2859c6ddb7604a8c944495724b2222fed3
kwanza <kwanza@sirenia.eu>
2018-06-13 08:11:12 +0000

    Committed via Provide invocation

--- flow/bcftkump6eb000euuck0/descriptive/code.js
+++ flow/bcftkump6eb000euuck0/descriptive/code.js
@@ -2,7 +2,9 @@
  * Describe this function...
  */
 function doSomething() {
-  for (var count = 0; count < 10; count++) {
-    Flow.run("Create PO", {});
-  }
+}
+
+
+for (var count = 0; count < 10; count++) {
+  Flow.run("Create PO", {});
 }
