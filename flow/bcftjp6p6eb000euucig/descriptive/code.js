80ef44ce14a1a20f3dc67b36de31faf64dbc67fb
kwanza <kwanza@sirenia.eu>
2018-06-13 09:33:31 +0000

    Committed via Provide invocation

--- flow/bcftjp6p6eb000euucig/descriptive/code.js
+++ flow/bcftjp6p6eb000euucig/descriptive/code.js
@@ -1,4 +1,4 @@
-for (var count = 0; count < ; count++) {
+for (var count = 0; count < 2; count++) {
   Notification.show('info', 'Its complicated', 'Round '+count, { severity: 'INFO', timeout: 5 });
   if(Fields["btn_Message_ok"].exists()){
     Notification.show('info', 'btn_Message_ok exists', 'Round '+count, { severity: 'INFO', timeout: 5 });
