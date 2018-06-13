fda5fad9b1ddb852260502f4f1f89b87cd1c128c
kwanza <kwanza@sirenia.eu>
2018-06-13 12:26:35 +0000

    Committed via Provide invocation

--- flow/bcge77mp6eb000euucu0/descriptive/code.js
+++ flow/bcge77mp6eb000euucu0/descriptive/code.js
@@ -14,7 +14,7 @@ Sticky.open(
       },
       {
         type: 'HTML',
-        source: '<h2>Big header</h1><h2>Smaller header</h2>',
+        source: '<h2>Smaller header</h2>',
         height: 50
       }
     ]
