e5cc1347e53f44858f36df822bf88787a42f9089
kwanza <kwanza@sirenia.eu>
2018-06-13 12:26:20 +0000

    Committed via Provide invocation

--- flow/bcge77mp6eb000euucu0/descriptive/code.js
+++ flow/bcge77mp6eb000euucu0/descriptive/code.js
@@ -11,6 +11,16 @@ Sticky.open(
       { 
         type: 'GIF',
         source: 'http://gifs.com/cat' 
+      },
+      {
+        type: 'HTML',
+        source: '<h1>Big header</h1><h2>Smaller header</h2>',
+        height: 50
+      },
+      {
+        type: 'LINK',
+        link: 'http://sirenia.eu',
+        prefix: 'Go to ', text: 'Sirenia', suffix: ' now'
       }
     ]
   }
