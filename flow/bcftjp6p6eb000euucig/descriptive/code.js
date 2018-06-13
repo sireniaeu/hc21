23239917a915901f45456a1ba058e2273d82d75d
kwanza <kwanza@sirenia.eu>
2018-06-13 11:32:20 +0000

    Committed via Provide invocation

--- flow/bcftjp6p6eb000euucig/descriptive/code.js
+++ flow/bcftjp6p6eb000euucig/descriptive/code.js
@@ -1,12 +1,12 @@
 //Debug.ger(Fields["btn_Message_ok"].inspect());
 //Fields["btn_Message_ok"].highlight();
 
-//for (var count = 0; count < 2; count++) {
-//  Notification.show('info', 'Its complicated', 'Round '+count, { severity: 'INFO', timeout: 5 });
+for (var count = 0; count < 2; count++) {
+  Notification.show('info', 'Its complicated', 'Round '+count, { severity: 'INFO', timeout: 5 });
   if(Fields["btn_Message_ok"].exists()){
     Notification.show('info', 'btn_Message_ok exists', '', { severity: 'INFO', timeout: 5 });
-    //Fields["btn_Message_ok"].highlight();
-    //break;
+    Fields["btn_Message_ok"].highlight();
+    break;
   }
   //Flow.run("Cancel Record", {});
-//}*/
+}
