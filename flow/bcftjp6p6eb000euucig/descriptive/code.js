f3adeded180113ffa4c70237399f2632155ac8d7
kwanza <kwanza@sirenia.eu>
2018-06-13 09:29:39 +0000

    Committed via Provide invocation

--- flow/bcftjp6p6eb000euucig/descriptive/code.js
+++ flow/bcftjp6p6eb000euucig/descriptive/code.js
@@ -1,5 +1,5 @@
 for (var count = 0; count < 10; count++) {
-  Notification.show('warn', 'Its complicated', 'Something broke down', { severity: 'WARN', timeout: 5 });
+  Notification.show('info', 'Its complicated', 'Something broke down', { severity: 'WARN', timeout: 5 });
   if(Fields["btn_Message_ok"].exists()){
     Fields["btn_Message_ok"].highlight();
     break;
