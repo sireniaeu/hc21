a7192e0db6d13d3f6ef2f80176162fe3e76fac21
kwanza <kwanza@sirenia.eu>
2018-06-13 08:50:23 +0000

    Committed via Provide invocation

--- flow/bcftkump6eb000euuck0/descriptive/code.js
+++ flow/bcftkump6eb000euuck0/descriptive/code.js
@@ -1,15 +1,9 @@
-var args;
-
 /**
  * Describe this function...
  */
 function doSomething() {
 }
 
-
-Fields["table_datagrid"].editCell('', '', '', {});
-
-args = 0;
 
 for (var count = 0; count < 10; count++) {
   Flow.run("Create PO", {});
