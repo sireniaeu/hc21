856da69aed13a9901fe9019279fc8377fc61ce3a
kwanza <kwanza@sirenia.eu>
2018-06-14 11:58:45 +0000

    Committed via Provide invocation

--- flow/bcftjp6p6eb000euucig/descriptive/code.js
+++ flow/bcftjp6p6eb000euucig/descriptive/code.js
@@ -4,6 +4,7 @@
 Fields["list_select_status"].select("Canceled");
 Wait.forMilliseconds(1000);
 Fields["btn_displayReport_optic"].click();
+Notification.show('info', 'All POs have been cancelled', '', { severity: 'INFO', timeout: 5 });
 Wait.forMilliseconds(500);
 
 for (var count = 0; count < 1000; count++) {
