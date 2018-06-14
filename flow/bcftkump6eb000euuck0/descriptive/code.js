eac9bc361cb087fb62218063f836e55b3f42b9dd
kwanza <kwanza@sirenia.eu>
2018-06-14 13:44:06 +0000

    Committed via Provide invocation

--- flow/bcftkump6eb000euuck0/descriptive/code.js
+++ flow/bcftkump6eb000euuck0/descriptive/code.js
@@ -1,6 +1,6 @@
 
 //Flow.run('showSticky', {});
-Notification.show('info', 'Its complicated', 'Round '+count, { severity: 'INFO', timeout: 500000 });
+Notification.show('info', 'Cr', 'Round '+count, { severity: 'INFO', timeout: 500000 });
 
 for (var count = 0; count < 10; count++) {
   Flow.run("Create PO", {});
