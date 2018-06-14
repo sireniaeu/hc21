601f2647514f2038cf6f1c22dc74eb7476de1720
kwanza <kwanza@sirenia.eu>
2018-06-14 12:11:50 +0000

    Committed via Provide invocation

--- flow/bcftjp6p6eb000euucig/descriptive/code.js
+++ flow/bcftjp6p6eb000euucig/descriptive/code.js
@@ -4,6 +4,7 @@ Fields["btn_Message_ok"].highlight();
 Fields["list_select_status"].select("Canceled");
 Wait.forMilliseconds(1000);
 Fields["btn_displayReport_optic"].click();
+
 Notification.show('info', 'Dispaly Report Clicked', '', { severity: 'INFO', timeout: 5 });
 Wait.forMilliseconds(1000);
 
