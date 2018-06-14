01a177eaf4d9c001c2379e91c2fbc978f6d11938
kwanza <kwanza@sirenia.eu>
2018-06-14 12:53:59 +0000

    Committed via Provide invocation

--- flow/blocklyTest/descriptive/code.js
+++ flow/blocklyTest/descriptive/code.js
@@ -1,1 +1,1 @@
-var args = {ref_no: Date.now(), add_to_address: true, remarks: (String('Something here. ') + String(Date.now())), items: [{name: null, amount: null, price: null}, null, null]};
+var args = {ref_no: Date.now(), add_to_address: true, remarks: (String('Something here. ') + String(Date.now())), items: [{name: '', amount: null, price: null}, null, null]};
