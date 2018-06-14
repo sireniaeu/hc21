88f6d7e127ac3abe183aa7c321e06d4ae98bc98d
kwanza <kwanza@sirenia.eu>
2018-06-14 13:07:36 +0000

    Committed via Provide invocation

--- flow/blocklyTest/descriptive/code.js
+++ flow/blocklyTest/descriptive/code.js
@@ -4,7 +4,5 @@ var args = {ref_no: Date.now(), add_to_address: true, remarks: (String('Somethin
 sendKeys('+^o');
 Fields["btn_create_po"].click({});
 sendKeys('{TAB}');
-for (i = 0; 0 < args.length; ) {
+for (i = 0; i < args.length; ) {
 }
-
-null;
