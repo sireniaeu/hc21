9990ec620b8149644edd9f952a828c939745b160
kwanza <kwanza@sirenia.eu>
2018-06-14 13:19:44 +0000

    Committed via Provide invocation

--- flow/blocklyTest/descriptive/code.js
+++ flow/blocklyTest/descriptive/code.js
@@ -9,7 +9,7 @@ for (var i = 0, item = null; i < args.length; i = i + 1) {
   item = args.items[i];
   sendKeys(item['name']);
   sendKeys('{TAB}');
-  sendKeys('{TAB}');
   sendKeys(item['amount']);
   sendKeys('{TAB 3}');
+  sendKeys('{TAB}');
 }
