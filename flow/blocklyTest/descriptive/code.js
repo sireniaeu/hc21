f63a4ca923496200afa32d2d4bc631705af3e319
kwanza <kwanza@sirenia.eu>
2018-06-14 13:17:27 +0000

    Committed via Provide invocation

--- flow/blocklyTest/descriptive/code.js
+++ flow/blocklyTest/descriptive/code.js
@@ -1,3 +1,4 @@
+// These are the parameters detailing exactly what should be entered into the system
 var args = {ref_no: Date.now(), add_to_address: true, remarks: (String('Something here. ') + String(Date.now())), items: [{name: 'One of these', amount: 1, price: 100}, {name: 'Two of these', amount: 2, price: 50}, {name: 'Some of those', amount: 1337, price: 33}]}, sendKeys = function(key) {
   Window['sendKeys'](key);
 };
