db18eeda345ccc723262483516db55794786ce66
kwanza <kwanza@sirenia.eu>
2018-06-14 14:01:28 +0000

    Committed via Provide invocation

--- flow/img-exist/descriptive/code.js
+++ flow/img-exist/descriptive/code.js
@@ -1,2 +1,2 @@
 
-Notification.show('info', Fields["btn_Message_ok"].exist(), 'Round '+count, { severity: 'INFO', timeout: 120 });
+Notification.show('info', Fields["btn_Message_ok"].exist(), '', { severity: 'INFO', timeout: 120 });
