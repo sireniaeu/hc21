67c4ed8d94b6b9bb9b7dd00708683318c9b5816b
kwanza <kwanza@sirenia.eu>
2018-06-14 13:29:16 +0000

    Committed via Provide invocation

--- flow/blocklyTest/descriptive/code.js
+++ flow/blocklyTest/descriptive/code.js
@@ -26,3 +26,6 @@ Fields["btn_ok"].click({});
 Fields["bnt_cancel"].click({});
 sendKeys('^%o');
 Wait.forMilliseconds(500);
+
+// Need support for input API method - and inputNative
+Fields["input_CommentstextBox"].input(args.remarks);
