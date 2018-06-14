6a68d25408ed4270885940a8590fbb528cb03f46
martin <martin@sirenia.eu>
2018-06-14 15:04:35 +0200

    ewe

--- flow/Cancel-all/descriptive/code.js
+++ flow/Cancel-all/descriptive/code.js
@@ -6,7 +6,7 @@ Wait.forMilliseconds(1000);
 Fields["btn_displayReport_optic"].click();
 
 Notification.show('info', 'Dispaly Report Clicked', '', { severity: 'INFO', timeout: 5 });
-Wait.forMilliseconds(2000);
+Wait.forMilliseconds(3000);
 
 for (var count = 0; count < 1000; count++) {
   Notification.show('info', 'Canceling Records', 'Round '+count, { severity: 'INFO', timeout: 5 });
