74063a7c7a057a22b269d0a71bf337409ce6a726
kwanza <kwanza@sirenia.eu>
2018-06-14 11:46:35 +0000

    Committed via Provide invocation

--- flow/bcfscq6p6eb000euuc8g/descriptive/code.js
+++ flow/bcfscq6p6eb000euuc8g/descriptive/code.js
@@ -3,7 +3,7 @@ Fields["list_select_status"].select("Canceled");
 Wait.forMilliseconds(1000);
 //Fields["btn_displayReport"].click();
 Fields["btn_displayReport_optic"].click();
-Wait.forMilliseconds(500);
+Wait.forMilliseconds(1500);
 Fields["btn_displayRecord"].click();
 Wait.forMilliseconds(500);
 Fields["btn_cancelRecord"].click();
