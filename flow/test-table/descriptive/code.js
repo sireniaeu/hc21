983536fb2564b922b08e59ea25055bfebf29d0f9
kwanza <kwanza@sirenia.eu>
2018-06-18 11:28:57 +0000

    Committed via Provide invocation

--- flow/test-table/descriptive/code.js
+++ flow/test-table/descriptive/code.js
@@ -1,5 +1,5 @@
 //Select Account. TODO:
 Fields["table_datagrid"].highlight();
 //Wait.forMilliseconds(1000);
-Fields["table_datagrid"].editcell();
+Fields["table_datagrid"].editcell("");
 Debug.ger(Fields["table_datagrid"]);
