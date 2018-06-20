3e4f53004d3775ef5da6d0f547ba164e0efbcbdd
kwanza <kwanza@sirenia.eu>
2018-06-20 10:05:26 +0000

    Committed via Provide invocation

--- flow/createPO/descriptive/code.js
+++ flow/createPO/descriptive/code.js
@@ -28,6 +28,7 @@ Window.sendKeys(args.product);
 Wait.forMilliseconds(500);
 Window.sendKeys("{TAB}");
 Window.sendKeys(args.taxcode);
+Wait.forMilliseconds(500);
 Window.sendKeys("{TAB 2}");
 Window.sendKeys(args.price);
 Window.sendKeys("{TAB}");
