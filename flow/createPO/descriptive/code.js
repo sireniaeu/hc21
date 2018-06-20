da6aacaa449d4d86e6c10c45b6687501d14fcd6b
kwanza <kwanza@sirenia.eu>
2018-06-20 10:13:43 +0000

    Committed via Provide invocation

--- flow/createPO/descriptive/code.js
+++ flow/createPO/descriptive/code.js
@@ -40,6 +40,6 @@ Window.sendKeys("{TAB}");
 Window.sendKeys(args.glcode);
 
 Fields["tableItem_OrderLine"].click();
-Window.sendKeys("{TAB}");
+Window.sendKeys("{DEL}");
 Window.sendKeys(args.glcode);
 
