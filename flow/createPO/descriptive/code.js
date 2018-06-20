2ebe7c9e3391a86fb42e2837186c5db8a23886c7
kwanza <kwanza@sirenia.eu>
2018-06-20 09:13:13 +0000

    Committed via Provide invocation

--- flow/createPO/descriptive/code.js
+++ flow/createPO/descriptive/code.js
@@ -1,5 +1,5 @@
 Fields["mainTreeView"].highlight();
 //Debug.ger(Fields["mainTreeView"].inspect());
 Window.sendKeys("{RIGHT}");
-Window.sendKeys("{RIGHT}");
+Window.sendKeys("{DOWN}");
 Window.sendKeys("{RIGHT}");
