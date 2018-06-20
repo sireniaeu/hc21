4768a71330cefda05b81eceb0b3abd9f69d72078
kwanza <kwanza@sirenia.eu>
2018-06-20 11:14:25 +0000

    Committed via Provide invocation

--- flow/createPO/descriptive/code.js
+++ flow/createPO/descriptive/code.js
@@ -15,7 +15,7 @@ Fields["treeSearch"].click();
 Window.sendKeys("Purchase Ordering Document Entry");
 Wait.forMilliseconds(1000);
 Fields["treeItem_PODE"].doubleClick();
-//Fields["btn_PO_Edit"].click();
+Fields["btn_PO_Edit"].click();
 
 Fields["input_scode"].click();
 Window.sendKeys(args.scode);
