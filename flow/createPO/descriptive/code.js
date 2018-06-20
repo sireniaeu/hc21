7c2375d99575b161e533674e5f4934f52e77ca6d
kwanza <kwanza@sirenia.eu>
2018-06-20 10:55:17 +0000

    Committed via Provide invocation

--- flow/createPO/descriptive/code.js
+++ flow/createPO/descriptive/code.js
@@ -43,12 +43,11 @@ Fields["tableItem_OrderLine"].click();
 Wait.forMilliseconds(2000);
 Fields["tableItem_OrderLine_form"].click();
 Wait.forMilliseconds(2000);
-Fields["tableItem_OrderLine_form"].click();
-Wait.forMilliseconds(2000);
 Window.sendKeys("IM HERE", {focus:false});
 Window.sendKeys("{BACKSPACE 40}", {focus:false});
 Wait.forMilliseconds(1000);
-Window.sendKeys(args.orderline);
+Window.sendKeys(args.orderline, {focus:false});
+
 
 
 
